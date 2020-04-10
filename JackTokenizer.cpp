4528fce87fdd80e2ffd7351aef745bdf6a262cd8
shinmy <shinmy.mt555@gmail.com>
2020-04-11 03:38:33 +0900

    JackTokenizer hasMoreCommandの作成

--- JackTokenizer.cpp
+++ JackTokenizer.cpp
@@ -2,3 +2,45 @@
 
 JackTokenizer::JackTokenizer(std::ifstream &jackfile)
     : ifs{jackfile}, token("") {}
+
+bool JackTokenizer::hasMoreTokens() {
+  token.clear();
+  while (char word = ifs.get()) {
+    if (word == ' ') continue;
+    if (word == '/') {
+      word = ifs.get();
+
+      // /**/パターン
+      if (word == '*') {
+        word = ifs.get();
+        while (1) {
+          while (word != '*') {
+            word = ifs.get();
+          }
+          word = ifs.get();
+          if (word == '/') break;
+          continue;
+        }
+        continue;
+      }
+
+      // //パターン
+      if (word == '/') {
+        while (word != '\n') {
+          word = ifs.get();
+        }
+        continue;
+      }
+
+      // 除算演算子
+      token += word;
+      return true;
+    }
+
+    token += word;
+    return true;
+  }
+
+  // eof
+  return false;
+}
