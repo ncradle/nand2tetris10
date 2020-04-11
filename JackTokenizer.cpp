1e060908d46affdfc5f169e63363d6cc30223e11
shinmy <shinmy.mt555@gmail.com>
2020-04-11 22:55:38 +0900

    advanceの作成とhasMoreCommandの修正
    一部のTokenは空白やスペースのトリムだけでは分割できず、
    文字の先読みをしないと分割できない
    ex) main() を main ( )にする
    そのために、advanceに先読みを実装した都合上
    hasMoreCommandを修正した

--- JackTokenizer.cpp
+++ JackTokenizer.cpp
@@ -1,14 +1,22 @@
 #include "JackTokenizer.h"
 
+#include <cctype>
+
+#include "SymbolReservedWord.h"
+
 JackTokenizer::JackTokenizer(std::ifstream &jackfile)
     : ifs{jackfile}, initial_token(0), token("") {}
 
 bool JackTokenizer::hasMoreTokens() {
+  if (initial_token != 0) return true;
+  initial_token = 0;
   while (1) {
     char word = ifs.get();
     // eof
     if (ifs.eof()) return false;
     if (word == ' ') continue;
+    if (word == '\n') continue;
+    if (word == '\t') continue;
     if (word == '/') {
       word = ifs.get();
 
@@ -42,4 +50,38 @@ bool JackTokenizer::hasMoreTokens() {
     initial_token = word;
     return true;
   }
+}
+
+void JackTokenizer::advance() {
+  token = initial_token;
+  // symbolの場合
+  if (SymbolReserverdWord.contains(initial_token)) {
+    initial_token = 0;
+    return;
+  }
+
+  // stringConstantの場合
+  if (initial_token == '\"') {
+    char word = ifs.get();
+    while (word != '\"') {
+      token += word;
+      word = ifs.get();
+    }
+    token += word;
+    initial_token = 0;
+    return;
+  }
+
+  char word = ifs.get();
+  while (isalnum(word) | (word == '_')) {
+    token += word;
+    word = ifs.get();
+    initial_token = 0;
+  }
+
+  if (SymbolReserverdWord.contains(word)) {
+    initial_token = word;
+    return;
+  }
+  initial_token = 0;
 }
